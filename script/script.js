ad8689c67825569d54eb6ff77fa03852708142c6
Jakob Larsen <jakoblarsen10000@users.noreply.github.com>
2021-02-28 11:47:07 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -42,9 +42,6 @@ checkList3.getElementsByClassName("anchor")[0].onclick = function (evt) {
 
 function activeFunc(id) {
   document.querySelector("#" + id).classList.add("dateActive");
-  if ("dateActive" === "true") {
-    document.querySelector("#" + id).classList.remove("dateActive");
-  }
 }
 
 function activeFuncTime(id) {
